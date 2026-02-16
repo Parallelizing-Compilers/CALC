2fdb9ec0e0b7488af7ba45f69d91a09af5887f08
Willow Ahrens <willow@csail.mit.edu>
2026-02-16 18:05:14 -0500

    add parse solution

--- src/calc/parse.py
+++ src/calc/parse.py
@@ -1,14 +1,11 @@
 from lark import Lark, Tree
 import calc.calc_lang as calc_lang
 
-# Your grammar here! Feel free to modify the below example grammar as much as
-# you want, or replace it entirely.
 lark_parser = Lark("""
     %import common.CNAME
     %import common.SIGNED_INT
     %import common.SIGNED_FLOAT
     %ignore " "           // Disregard spaces in text
-
 
     literal: float_literal | int_literal
     int_literal: SIGNED_INT
@@ -16,8 +13,14 @@ lark_parser = Lark("""
 
     variable: CNAME
 
-    expr: add_expr
-    add_expr: (literal "+")? literal
+    // PEMDAS precedence (lowest to highest)
+    expr: addsub_expr
+    addsub_expr: add_expr | sub_expr
+    sub_expr: (addsub_expr "-")? mul_expr
+    add_expr: (addsub_expr "+")? mul_expr
+    mul_expr: (mul_expr "*")? power_expr
+    power_expr: primary ("^" power_expr)?
+    primary: literal | variable | "(" expr ")"
 
     start: expr
 """)
@@ -28,12 +31,39 @@ def parse(expr: str) -> calc_lang.CalcLangExpression:
 
 def _parse(tree: Tree) -> calc_lang.CalcLangExpression:
     match tree:
-        case Tree("start" | "literal" | "expr" | "add_expr", [expr]):
+        case Tree(
+            "start" |
+            "expr" |
+            "addsub_expr" |
+            "primary" |
+            "literal" |
+            "sub_expr" |
+            "power_expr"|
+            "add_expr" |
+            "mul_expr",
+            [expr]
+        ):
             return _parse(expr)
+        case Tree("sub_expr", [left, right]):
+            left_expr = _parse(left)
+            right_expr = _parse(right)
+            return calc_lang.Sub(left_expr, right_expr)
         case Tree("add_expr", [left, right]):
             left_expr = _parse(left)
             right_expr = _parse(right)
             return calc_lang.Add(left_expr, right_expr)
+        case Tree("mul_expr", [left, right]):
+            left_expr = _parse(left)
+            right_expr = _parse(right)
+            return calc_lang.Mul(left_expr, right_expr)
+        case Tree("power_expr", [base, exponent]):
+            base_expr = _parse(base)
+            exponent_expr = _parse(exponent)
+            return calc_lang.Pow(base_expr, exponent_expr)
+        case Tree("primary", [expr]):
+            return _parse(expr)
+        case Tree("variable", [name]):
+            return calc_lang.Variable(name.value)
         case Tree("int_literal", [value]):
             return calc_lang.Literal(int(value))
         case Tree("float_literal", [value]):
